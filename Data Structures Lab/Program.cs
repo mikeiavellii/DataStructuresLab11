9ef00357b7f4a0cc62abdc9447e1af8980fdbfc5
Mike <mike.blackburn.541@gmail.com>
2023-02-02 17:12:16 -0500

    first

---
+++ Data Structures Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Write("Enter a word to reverse: ");
+string entry = Console.ReadLine().Trim();
+Console.WriteLine($"Your word in reverse is: {Reverse(entry)}."); 
+
+
+static string Reverse(string s)
+{
+  string final = "";
+  Stack<char> stack = new Stack<char>();
+  foreach (char p in s)
+  {
+    stack.Push(p);
+  }
+  for (int i = 1; i <= s.Length; i++)
+  {
+    final += stack.Pop();
+  }
+  return final;
+}
+
